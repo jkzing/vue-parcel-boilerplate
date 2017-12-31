22429eb2f3f8a0749b351f92f301e37cbaf545d7
JingkaiZhao <jingkai.zhao@foxmail.com>
2017-12-31 16:07:59 +0800

    feat: add vue style helloworld

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,28 @@
 <template>
-  <h1>hello</h1>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <HelloWorld />
+  </div>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld'
+
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    HelloWorld
+  }
 }
 </script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
